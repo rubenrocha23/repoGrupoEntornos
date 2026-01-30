a967719394e6fc7ce46aa2a95ebc088727d28cb9
rubenrocha23 <rubenrocha.contact@gmail.com>
2026-01-30 12:09:57 +0100

    Implement main calculator functionality
    Este es el main sobre el que vamos a trabajar

---
+++ main.py
@@ -0,0 +1,30 @@
+from operaciones.operaciones_basicas import sumar, restar
+from operaciones.operaciones_avanzadas import multiplicar, dividir
+from operaciones.utilidades import pedir_numeros, mostrar_menu
+
+
+def main():
+    while True:
+        mostrar_menu()
+        opcion = input("Elige una opción: ")
+
+        if opcion == "5":
+            print("Saliendo de la calculadora...")
+            break
+
+        num1, num2 = pedir_numeros()
+
+        if opcion == "1":
+            print("Resultado:", sumar(num1, num2))
+        elif opcion == "2":
+            print("Resultado:", restar(num1, num2))
+        elif opcion == "3":
+            print("Resultado:", multiplicar(num1, num2))
+        elif opcion == "4":
+            print("Resultado:", dividir(num1, num2))
+        else:
+            print("Opción no válida")
+
+
+if __name__ == "__main__":
+    main()
