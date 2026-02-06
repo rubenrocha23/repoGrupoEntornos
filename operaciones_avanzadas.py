a771323896d294c77f36401227f7f465e79574e6
usuario <rubenrocha.contact@gmail.com>
2026-02-06 12:43:21 +0100

    operaciones_avanzadas

---
+++ operaciones_avanzadas.py
@@ -0,0 +1,10 @@
+# operaciones_avanzadas.py
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: no se puede dividir entre 0"
+    return a / b
